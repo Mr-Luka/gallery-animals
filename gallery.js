6d464892a5187ffdd0ea309e7a690f7934365c10
Luka <luka.luka.luka93@gmail.com>
2024-04-16 14:37:56 -0700

    Fixed the buttons on each side and fixed the js

--- gallery.js
+++ gallery.js
@@ -1,38 +1,38 @@
-
-function Gallery(gallery) {
+function Gallery (gallery) {
     if(!gallery) {
-        throw new Error("No Gallery found!");
+        throw new Error("No gallery found!");
     }
-    console.log(gallery)
-
     const images = Array.from(gallery.querySelectorAll("img"));
     const modal = document.querySelector(".modal");
     const prevButton = modal.querySelector(".prev");
     const nextButton = modal.querySelector(".next");
     let currentImage;
-    function openModal (){
-        console.info("opening modal...");
-        if(modal.matches(".open")) {
-            console.info("Modal already open");
-            return;
-        } 
+    
+function openModal () {
+    if (modal.matches(".open")){
+        console.log("Modal is already open");
+        return;
+    } else {
         modal.classList.add("open");
     }
+}
+
     function showImage(el) {
-        if(!el) {
-            console.info("No Image to show");
-            return;
-        }
-        console.log(el);
         modal.querySelector("img").src = el.src;
         modal.querySelector("h4").textContent = el.title;
         modal.querySelector("figure p").textContent = el.dataset.description;
         currentImage = el;
-        openModal()
+        openModal();
+
     }
 
-    images.forEach(image => image.addEventListener("click", e=> showImage(e.currentTarget)))
+    images.forEach(image=> image.addEventListener("click", (e)=> showImage(e.currentTarget)))
+
+
+
+    
 }
+
 
 
 const gallery1 = Gallery(document.querySelector(".gallery1"));
